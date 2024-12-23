1a3a52a1d513383b9e97d2dd1766d3d895406967
Gaurav Yadav <gaurav@macbook.local>
2024-12-23 23:02:57 +0530

    init

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,11 @@
-import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
   {
@@ -14,12 +13,7 @@ export default [
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
-  js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
+  ...vueTsEslintConfig(),
   skipFormatting,
 ]
